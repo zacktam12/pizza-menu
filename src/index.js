44a93f5ec032a96fd2477e76acf544123691003e
hp <zekariastamiru12@gmail.com>
2024-10-16 00:12:21 +0300

    Working With Components, Props, and JSX

--- src/index.js
+++ src/index.js
@@ -1,8 +1,73 @@
-import react from "react";
+import react, { StrictMode } from "react";
 import ReactDom from "react-dom/client";
 
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
 function App() {
-  return <h1>Hello everybody</h1>;
+  return (
+    <div>
+      <h1>Hello everybody!</h1>
+      <Pizza />;
+    </div>
+  );
 }
+
+function Pizza() {
+  return (
+    <div>
+      <h2>Pizza Spinaci</h2>
+      <h2>Tomato, mozarella, spinach, and ricotta cheese</h2>;
+      <img src="pizzas/spinaci.jpg" alt="pizza spinaci"></img>;
+    </div>
+  );
+}
+// react v18
 const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
